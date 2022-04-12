c65c0162f02458e3b2f3390ddf8bd9233187deb7
Jake Walsh <jkwalsh127@gmail.com>
2022-04-11 20:11:43 -0700

    rearrange use of jQuery to instead create elements, give them classes, and give them text corresponding to output data, and then append these to the document

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,13 +30,50 @@ function getAbstractData() {
             var isSmtpValidBoolean = data.is_smtp_valid.value;
             var isSmtpValidString = data.is_smtp_valid.text;
 
-            $('#email-input').text('Email' + email);
-            $('#deliverability-output').text('Deliverability: ' + deliverability);
-            $('#quality-score-output').text('Quality score: ' + qualityScore);
-            $('#is-free-email').text("From Abstract's list of free email providers: " + isFreeEmailString);
-            $('#is-disposable-email').text("From Abstract's list of disposable email providers: " + isDisposableEmailString);
-            $('#is-role-email').text("Email for role rather than individual: " + isRoleEmailString);
-            $('#is-catchall-email').text("Email is a catchall for its domain: " +isCatchallEmailString);
-            $('#is-smtp-valid').text("Email SMTP check: " + isSmtpValidString);
+            // $('#email-input').text(email);
+            // $('#deliverability-output').text(deliverability);
+            // $('#quality-score-output').text('Quality score: ' + qualityScore);
+            // $('#is-free-email').text("From Abstract's list of free email providers: " + isFreeEmailString);
+            // $('#is-disposable-email').text("From Abstract's list of disposable email providers: " + isDisposableEmailString);
+            // $('#is-role-email').text("Email for role rather than individual: " + isRoleEmailString);
+            // $('#is-catchall-email').text("Email is a catchall for its domain: " +isCatchallEmailString);
+            // $('#is-smtp-valid').text("Email SMTP check: " + isSmtpValidString);
+
+            var outputEl = $('#output');
+            var emailInput = $('<h4>');
+            emailInput.text('Email: ' + email);
+            emailInput.attr('class', 'data-output')
+            var deliverabilityOutput = $('<h4>');
+            deliverabilityOutput.text('Deliverability: ' + deliverability);
+            deliverabilityOutput.attr('class', 'data-output')
+            var qualityScoreOutput = $('<h4>');
+            qualityScoreOutput.text('Quality score: ' + qualityScore);
+            qualityScoreOutput.attr('class', 'data-output')
+            var isFreeEmail = $('<h4>');
+            isFreeEmail.text("From Abstract's list of free email providers: " + isFreeEmailString);
+            isFreeEmail.attr('class', 'data-output')
+            var isDisposableEmail = $('<h4>');
+            isDisposableEmail.text("From Abstract's list of disposable email providers: " + isDisposableEmailString);
+            isDisposableEmail.attr('class', 'data-output')
+            var isRoleEmail = $('<h4>');
+            isRoleEmail.text("Email for role rather than individual: " + isRoleEmailString);
+            isRoleEmail.attr('class', 'data-output')
+            var isCatchallEmail = $('<h4>');
+            isCatchallEmail.text("Email is a catchall for its domain: " +isCatchallEmailString);
+            isCatchallEmail.attr('class', 'data-output')
+            var isSmtpValid = $('<h4>');
+            isSmtpValid.text("Email SMTP check: " + isSmtpValidString);
+            isSmtpValid.attr('class', 'data-output')
+
+            outputEl.append(emailInput);
+            outputEl.append(deliverabilityOutput);
+            outputEl.append(qualityScoreOutput);
+            outputEl.append(isFreeEmail);
+            outputEl.append(isDisposableEmail);
+            outputEl.append(isRoleEmail);
+            outputEl.append(isCatchallEmail);
+            outputEl.append(isSmtpValid);
         });
 };
+
+getAbstractData();
