a3ec441f573e91ca74b72a60dbb8bb6b8908d9ca
Jake Walsh <jkwalsh127@gmail.com>
2022-04-11 21:39:22 -0700

    add check to detect if email was submitted with a typo or in improper format

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,24 @@ function getAbstractData() {
         })
         .then(function (data) {
             console.log(data);
-
+            // if the Abstract API detects a type in the submission, it will provide an autocorrect solution,
+            // which will be provided as a recommendation to the user through a sweetalert.js notification
+            if (data.autocorrect != "") {
+                console.log(data.autocorrect);
+                swal("Error", "There was a typo detected. Maybe you meant" + data.autocorrect);
+                return;
+            // if the Abstract API detects the submission was in an invalid email format,
+            // this check will prompt the user to try again through a sweetalert.js notification
+            } else if (data.is_valid_format == false) {
+                swal("Error", "The email entered was not in a valid format. Please try again.");
+                return;
+            };
+            // if the checks where passed, the following code creates variables for the data retreived from the
+            // API request, creates h4 elements and assigns them classes as well as the corresponding data, and
+            // finally appends each one to the '#output' section of the html doc
             var email = data.email;
-            var autocorrect = data.autocorrect;
             var deliverability = data.deliverability;
             var qualityScore = data.quality_score;
-            var isValidFormatBoolean = data.is_valid_format.value;
-            var isValidFormatString = data.is_valid_format.text;
             var isFreeEmailBoolean = data.is_free_email.value;
             var isFreeEmailString = data.is_free_email.text;
             var isDisposableEmailBoolean = data.is_disposable_email.value;
@@ -29,15 +40,6 @@ function getAbstractData() {
             var isCatchallEmailString = data.is_catchall_email.text;
             var isSmtpValidBoolean = data.is_smtp_valid.value;
             var isSmtpValidString = data.is_smtp_valid.text;
-
-            // $('#email-input').text(email);
-            // $('#deliverability-output').text(deliverability);
-            // $('#quality-score-output').text('Quality score: ' + qualityScore);
-            // $('#is-free-email').text("From Abstract's list of free email providers: " + isFreeEmailString);
-            // $('#is-disposable-email').text("From Abstract's list of disposable email providers: " + isDisposableEmailString);
-            // $('#is-role-email').text("Email for role rather than individual: " + isRoleEmailString);
-            // $('#is-catchall-email').text("Email is a catchall for its domain: " +isCatchallEmailString);
-            // $('#is-smtp-valid').text("Email SMTP check: " + isSmtpValidString);
 
             var outputEl = $('#output');
             var emailInput = $('<h4>');
@@ -76,4 +78,44 @@ function getAbstractData() {
         });
 };
 
-getAbstractData();
+// This function is a representation of the getAbstractData fxn but will not query the API, 
+// thereby saving available monthly requests when testing
+function getAbstractDataNoQuery() {
+
+            var outputEl = $('#output');
+            var emailInput = $('<h4>');
+            emailInput.text('Email: jkwalsh127@gmail.com');
+            emailInput.attr('class', 'data-output')
+            var deliverabilityOutput = $('<h4>');
+            deliverabilityOutput.text('Deliverability: DELIVERABLE');
+            deliverabilityOutput.attr('class', 'data-output')
+            var qualityScoreOutput = $('<h4>');
+            qualityScoreOutput.text('Quality score: 0.70');
+            qualityScoreOutput.attr('class', 'data-output')
+            var isFreeEmail = $('<h4>');
+            isFreeEmail.text("From Abstract's list of free email providers: TRUE");
+            isFreeEmail.attr('class', 'data-output')
+            var isDisposableEmail = $('<h4>');
+            isDisposableEmail.text("From Abstract's list of disposable email providers: FALSE");
+            isDisposableEmail.attr('class', 'data-output')
+            var isRoleEmail = $('<h4>');
+            isRoleEmail.text("Email for role rather than individual: FALSE");
+            isRoleEmail.attr('class', 'data-output')
+            var isCatchallEmail = $('<h4>');
+            isCatchallEmail.text("Email is a catchall for its domain: FALSE");
+            isCatchallEmail.attr('class', 'data-output')
+            var isSmtpValid = $('<h4>');
+            isSmtpValid.text("Email SMTP check: TRUE");
+            isSmtpValid.attr('class', 'data-output')
+
+            outputEl.append(emailInput);
+            outputEl.append(deliverabilityOutput);
+            outputEl.append(qualityScoreOutput);
+            outputEl.append(isFreeEmail);
+            outputEl.append(isDisposableEmail);
+            outputEl.append(isRoleEmail);
+            outputEl.append(isCatchallEmail);
+            outputEl.append(isSmtpValid);
+};
+
+getAbstractDataNoQuery();
