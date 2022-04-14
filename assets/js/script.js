fbd546bace056103ac225e95558cd3e887768ad1
Jonathan Bridge <jvbridge@gmail.com>
2022-04-13 20:04:07 -0700

    added dummy data for the have I been pwned API

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,15 +141,50 @@ function getAbstractDataNoQuery(userInput) {
 }
 
 document.addEventListener("click", function(event) {
-<<<<<<< HEAD
-    if (event.target.matches("#emailBtn")) {
-        var formInput = document.querySelector("#emailInput")
-=======
     if (event.target.matches("#email-btn")) {
         var formInput = document.querySelector("#email-input")
->>>>>>> 6b0f0a82ca2a226661035385d838054f7fd7f7ac
         userInput = formInput.value;
         console.log(userInput);
         getAbstractDataNoQuery(userInput);
     }
 });
+
+/**
+ * A dummy query for the have I been pwned API
+ */
+var PWNED_DUMMY_DATA = [
+    {
+    "Name":"Adobe",
+    "Title":"Adobe",
+    "Domain":"adobe.com",
+    "BreachDate":"2013-10-04",
+    "AddedDate":"2013-12-04T00:00Z",
+    "ModifiedDate":"2013-12-04T00:00Z",
+    "PwnCount":152445165,
+    "Description":"In October 2013, 153 million Adobe accounts were breached with each containing an internal ID, username, email, <em>encrypted</em> password and a password hint in plain text. The password cryptography was poorly done and <a href=\"http://stricture-group.com/files/adobe-top100.txt\" target=\"_blank\" rel=\"noopener\">many were quickly resolved back to plain text</a>. The unencrypted hints also <a href=\"http://www.troyhunt.com/2013/11/adobe-credentials-and-serious.html\" target=\"_blank\" rel=\"noopener\">disclosed much about the passwords</a> adding further to the risk that hundreds of millions of Adobe customers already faced.",
+    "DataClasses":["Email addresses","Password hints","Passwords","Usernames"],
+    "IsVerified":true,
+    "IsFabricated":false,
+    "IsSensitive":false,
+    "IsRetired":false,
+    "IsSpamList":false,
+    "LogoPath":"https://haveibeenpwned.com/Content/Images/PwnedLogos/Adobe.png"
+    },
+    {
+    "Name":"BattlefieldHeroes",
+    "Title":"Battlefield Heroes",
+    "Domain":"battlefieldheroes.com",
+    "BreachDate":"2011-06-26",
+    "AddedDate":"2014-01-23T13:10Z",
+    "ModifiedDate":"2014-01-23T13:10Z",
+    "PwnCount":530270,
+    "Description":"In June 2011 as part of a final breached data dump, the hacker collective &quot;LulzSec&quot; <a href=\"http://www.rockpapershotgun.com/2011/06/26/lulzsec-over-release-battlefield-heroes-data\" target=\"_blank\" rel=\"noopener\">obtained and released over half a million usernames and passwords from the game Battlefield Heroes</a>. The passwords were stored as MD5 hashes with no salt and many were easily converted back to their plain text versions.",
+    "DataClasses":["Passwords","Usernames"],
+    "IsVerified":true,
+    "IsFabricated":false,
+    "IsSensitive":false,
+    "IsRetired":false,
+    "IsSpamList":false,
+    "LogoPath":"https://haveibeenpwned.com/Content/Images/PwnedLogos/BattlefieldHeroes.png"
+    }
+];
